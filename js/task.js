5d71bf213084ae60c04ac42139dfd054d214c337
khokan77 <shossain.khokon@gmail.com>
2025-03-01 12:09:51 +0600

    task updated

--- js/task.js
+++ js/task.js
@@ -1,8 +1,66 @@
 document.getElementById("task1").addEventListener("click", function (event) {
   event.preventDefault();
-  const text = document.getElementById("task-count").innerText;
-  document.getElementById("task-count").innerText = parseInt(text) - 1;
-  var elem = document.getElementById("task1");
-  elem.classList.remove("btn-active");
-  elem.classList.add("btn-disabled");
+  taskUpdatedOnClick(
+    "task-count-add",
+    "task-count-sub",
+    "task1",
+    "task1-title"
+  );
 });
+
+document.getElementById("task2").addEventListener("click", function (event) {
+  event.preventDefault();
+  taskUpdatedOnClick(
+    "task-count-add",
+    "task-count-sub",
+    "task2",
+    "task2-title"
+  );
+});
+
+document.getElementById("task3").addEventListener("click", function (event) {
+  event.preventDefault();
+  taskUpdatedOnClick(
+    "task-count-add",
+    "task-count-sub",
+    "task3",
+    "task3-title"
+  );
+});
+
+document.getElementById("task4").addEventListener("click", function (event) {
+  event.preventDefault();
+  taskUpdatedOnClick(
+    "task-count-add",
+    "task-count-sub",
+    "task4",
+    "task4-title"
+  );
+});
+
+document.getElementById("task5").addEventListener("click", function (event) {
+  event.preventDefault();
+  taskUpdatedOnClick(
+    "task-count-add",
+    "task-count-sub",
+    "task5",
+    "task5-title"
+  );
+});
+
+document.getElementById("task6").addEventListener("click", function (event) {
+  event.preventDefault();
+  taskUpdatedOnClick(
+    "task-count-add",
+    "task-count-sub",
+    "task6",
+    "task6-title"
+  );
+});
+
+document
+  .getElementById("task-clear")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    document.getElementById("task-list").innerHTML = "";
+  });
--- js/utility.js
+++ js/utility.js
@@ -23,3 +23,38 @@ function toggleElementenable(id) {
     elem.disabled = true;
   }
 }
+
+function taskUpdatedOnClick(add, sub, task, taskTitle) {
+  let countSub = document.getElementById(add);
+  countSub.innerText = parseInt(countSub.innerText) + 1;
+
+  let countAdd = document.getElementById(sub);
+  countAdd.innerText = parseInt(countAdd.innerText) - 1;
+
+  const tastText = document.getElementById(taskTitle).innerText;
+
+  var task = document.getElementById(task);
+  task.classList.remove("btn-active");
+  task.classList.add("btn-disabled");
+
+  const paragraph = document.createElement("p");
+
+  paragraph.classList.add(
+    "bg-[#F4F7FF]",
+    "p-4",
+    "rounded-lg",
+    "text-center",
+    "text-gray-600"
+  );
+
+  // Add text content
+  paragraph.textContent =
+    "You have complated the task " +
+    tastText +
+    " at " +
+    new Date().toLocaleTimeString();
+
+  document.getElementById("task-list").appendChild(paragraph);
+  const brek = document.createElement("br");
+  document.getElementById("task-list").appendChild(brek);
+}
