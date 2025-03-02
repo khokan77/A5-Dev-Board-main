183958603c1b31eba646d316232fbbe690393a18
khokan77 <shossain.khokan@gmail.com>
2025-03-02 14:45:24 +0600

    final updated

--- js/task.js
+++ js/task.js
@@ -1,3 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("current-date").textContent = new Date().toDateString();
+});
+
+
 document.getElementById("task1").addEventListener("click", function (event) {
   event.preventDefault();
   alert("Board Updated Successfully");
@@ -89,4 +94,43 @@ function changeTheme() {
     "border-2",
     "m-0"
   );
+}
+
+function taskUpdatedOnClick(add, sub, task, taskTitle) {
+  let countSub = document.getElementById(add);
+  countSub.innerText = parseInt(countSub.innerText) + 1;
+
+  let countAdd = document.getElementById(sub);
+  countAdd.innerText = parseInt(countAdd.innerText) - 1;
+
+  const tastText = document.getElementById(taskTitle).innerText;
+
+  var task = document.getElementById(task);
+  task.classList.remove("btn-active");
+  task.classList.add("btn-disabled");
+
+  const paragraph = document.createElement("p");
+
+  paragraph.classList.add(
+    "bg-[#F4F7FF]",
+    "p-4",
+    "rounded-lg",
+    "text-center",
+    "text-gray-600"
+  );
+
+  // Add text content
+  paragraph.textContent =
+    "You have complated the task " +
+    tastText +
+    " at " +
+    new Date().toLocaleTimeString();
+
+  document.getElementById("task-list").appendChild(paragraph);
+  const brek = document.createElement("br");
+  document.getElementById("task-list").appendChild(brek);
+
+  if(countAdd.innerText == "0"){
+    alert ("Congrats !! You have completed all the tasks");
+  }
 }
