fdc4ffbc6fa74bf6c289091743b433d71057a6be
khokan77 <shossain.khokon@gmail.com>
2025-03-02 17:07:13 +0600

    updated

--- js/task.js
+++ js/task.js
@@ -1,3 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("current-date").textContent =
+    new Date().toDateString();
+});
+
 document.getElementById("task1").addEventListener("click", function (event) {
   event.preventDefault();
   alert("Board Updated Successfully");
@@ -70,3 +75,62 @@ document
     event.preventDefault();
     document.getElementById("task-list").innerHTML = "";
   });
+
+function changeTheme() {
+  const colors = [
+    "bg-[#F4F7FF]",
+    "bg-blue-100",
+    "bg-green-100",
+    "bg-yellow-100",
+    "bg-red-100",
+  ];
+  document.body.className = colors[Math.floor(Math.random() * colors.length)];
+  document.body.classList.add(
+    "poppin-font",
+    "w-full",
+    "bg-cover",
+    "rounded-lg",
+    "bg-center",
+    "border-2",
+    "m-0"
+  );
+}
+
+function taskUpdatedOnClick(add, sub, task, taskTitle) {
+  let countSub = document.getElementById(add);
+  countSub.innerText = parseInt(countSub.innerText) + 1;
+
+  let countAdd = document.getElementById(sub);
+  countAdd.innerText = parseInt(countAdd.innerText) - 1;
+
+  const tastText = document.getElementById(taskTitle).innerText;
+
+  var task = document.getElementById(task);
+  task.classList.remove("btn-active");
+  task.classList.add("btn-disabled");
+
+  const paragraph = document.createElement("p");
+
+  paragraph.classList.add(
+    "bg-[#F4F7FF]",
+    "p-4",
+    "rounded-lg",
+    "text-center",
+    "text-gray-600"
+  );
+
+  // Add text content
+  paragraph.textContent =
+    "You have complated the task " +
+    tastText +
+    " at " +
+    new Date().toLocaleTimeString();
+
+  document.getElementById("task-list").appendChild(paragraph);
+  const brek = document.createElement("br");
+  document.getElementById("task-list").appendChild(brek);
+
+  if (countAdd.innerText == "0") {
+    alert("Congrats !! You have completed all the tasks");
+  }
+}
